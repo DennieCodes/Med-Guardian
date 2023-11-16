3f9639783c52bc7e4d69a2d306cd705cd2b54805
Dennie <oculareo@gmail.com>
2023-11-16 17:57:12 -0500

    Added placeholder classes, functions for medications

--- api/queries/medications.py
+++ api/queries/medications.py
@@ -2,4 +2,23 @@ from pydantic import BaseModel
 
 
 class MedicationRepository(BaseModel):
-    pass
+    def create(self):
+        pass
+
+    def get_all(self):
+        pass
+
+    def get_one(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update_quantity(self):
+        pass
+
+    def update_refill(self):
+        pass
--- api/routers/medications.py
+++ api/routers/medications.py
@@ -1,7 +1,48 @@
 from fastapi import APIRouter
 
-from models.medications import MedicationIn, MedicationOut, Error
-
-from authenticator import authenticator
+# from models.medications import MedicationIn, MedicationOut, Error
+# from authenticator import authenticator
 
 router = APIRouter()
+
+
+# CREATE MEDICATION
+@router.post("/api/medications")
+def create_medication():
+    pass
+
+
+# GET ALL MEDICATION
+@router.get("/api/medications")
+def get_all():
+    pass
+
+
+# GET MEDICATION
+@router.get("/api/medications/{medications_id}")
+def get_medication():
+    pass
+
+
+# UPDATE MEDICATION
+@router.put("/api/medications/{medications_id}")
+def update_medication():
+    pass
+
+
+# DELETE MEDICATION
+@router.delete("/api/medications/{medications_id}")
+def delete_medication():
+    pass
+
+
+# UPDATE MEDICATION QUANTITY
+@router.put("/api/medications/{medications_id}/quantity")
+def update_medication_quantity():
+    pass
+
+
+# UPDATE MEDICATION QUANTITY
+@router.put("/api/medications/{medications_id}/refill")
+def update_refill_quantity():
+    pass
