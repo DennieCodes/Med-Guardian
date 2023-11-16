5cf2017e3b656b8d142ac9f74c8d72639f88d1c9
Onkur Lal <onkur24lal@gmail.com>
2023-11-16 16:16:30 -0500

    fda api call to get drug list

---
+++ api/routers/third_party_calls.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from queries.third_party_calls import ThirdPartyQueries
+
+
+router = APIRouter()
+
+
+@router.get('/api/drug_list')
+def get_drug_list(repo: ThirdPartyQueries = Depends()):
+    return repo.get_drug_list()
+
+
+@router.get('api/users_drug_interactions')
+def get_users_drug_interactions():
+    pass
