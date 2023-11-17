b9fb54e4a14deaae32c1f5b54c73f7c2b533e0e9
mzinzun <zinzunmichael@gmail.com>
2023-11-16 18:50:36 -0800

    added model fields

--- api/models/medications.py
+++ api/models/medications.py
@@ -5,9 +5,38 @@ class Error(BaseModel):
     message: str
 
 
+class MedicationUpdateQty(BaseModel):
+    name: str
+    strength: str
+    dosage: int
+    frequency: int
+    quantity: int
+    refills: int
+    doctor_id: int
+    pharmacy_id: int
+    user_id: int
+
+
 class MedicationsIn(BaseModel):
     name: str
+    strength: str
+    dosage: int
+    frequency: int
+    quantity: int
+    refills: int
+    doctor_id: int
+    pharmacy_id: int
+    user_id: int
 
 
 class MedicationsOut(BaseModel):
+    id: int
     name: str
+    strength: str
+    dosage: int
+    frequency: int
+    quantity: int
+    refills: int
+    doctor_id: int
+    pharmacy_id: int
+    user_id: int
