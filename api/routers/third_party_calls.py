c380fc888d18780d5cfefff8208d394cb2be98a5
Onkur Lal <onkur24lal@gmail.com>
2023-11-20 12:47:10 -0500

    journal and 3rd party api call interactions

--- api/routers/third_party_calls.py
+++ api/routers/third_party_calls.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from queries.third_party_calls import ThirdPartyQueries
-
+from authenticator import authenticator
 
 router = APIRouter()
 
@@ -10,6 +10,9 @@ def get_drug_list(repo: ThirdPartyQueries = Depends()):
     return repo.get_drug_list()
 
 
-@router.get("api/users_drug_interactions")
-def get_users_drug_interactions():
-    pass
+@router.get("/api/users_drug_interactions")
+def get_users_drug_interactions(
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    repo: ThirdPartyQueries = Depends()
+):
+    return repo.get_interactions(account_data["id"])
