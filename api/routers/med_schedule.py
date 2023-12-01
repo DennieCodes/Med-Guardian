af43dba178e9e8d3605130e8d91a6f3011dd9747
Onkur Lal <onkur24lal@gmail.com>
2023-12-01 13:23:37 -0500

    backend for med events

--- api/routers/med_schedule.py
+++ api/routers/med_schedule.py
@@ -1,16 +1,28 @@
 from fastapi import APIRouter, Response, Depends
 from typing import Union, List
-from models.schedules import ScheduleIn, ScheduleOut, Error
-from queries.schedules import ScheduleRepository
+from models.schedules import EventIn, EventOut, Error
+from queries.schedules import EventsRepository
 from authenticator import authenticator
 
 router = APIRouter()
 
-@router('api/schedule/create', response_model=Union[ScheduleOut, Error])
+
+@router.post('/api/events', response_model=Union[List[EventOut], Error])
 def create(
-    schedule: ScheduleIn,
+    events: List[EventIn],
     response: Response,
-    repo: ScheduleRepository = Depends(),
     user: dict = Depends(authenticator.get_current_account_data),
-) -> ScheduleOut:
-    return repo.create(schedule)
+    repo: EventsRepository = Depends()
+):
+    events = repo.create(events, user["id"])
+    if not isinstance(events, list) or not all(isinstance(e, EventOut) for e in events):
+        response.status_code = 400
+    return events
+
+@router.get("/api/events", response_model=Union[List[EventOut], Error])
+def get_all(
+    user: dict = Depends(authenticator.get_current_account_data),
+    repo: EventsRepository = Depends()
+):
+    events = repo.get_all(user["id"])
+    return events
