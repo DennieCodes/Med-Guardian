d561bda39361086264e0e64d521c5f68f6ffeb82
Onkur Lal <onkur24lal@gmail.com>
2023-12-07 18:48:11 -0500

    reverted cors orginis

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
 from authenticator import authenticator
 from routers import (
     accounts,
@@ -15,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
